eade6233e0de9c7d221fb93b1cf9c23d99133ad1
unknown <terrencemccarthysnkrs@gmail.com>
2023-09-20 21:36:52 -0500

    completed 2nd section and started 3rd section

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -3,25 +3,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword () {
+
+  alert("Please add lowercase, uppercase, numeric, and special characters");
+
   var length = parseInt(prompt("Enter password length from 8 to 128 characters"));
 
   if (length < 8 || length > 128) {
     alert("Invalid Length");
+    return; 
+  } 
+
+  
+  var inputLowerCase = confirm("Would you like to include lower case characters?");
+  var inputUpperCase = confirm("Would you like to include upper case characters?");
+  var inputNum = confirm("Would you like to include numeric characters?");
+  var inputSpecialCharacters = confirm("Would you like to include special characters?");
+
+  if (!inputLowerCase && !inputUpperCase && !inputNum && !inputSpecialCharacters) {
+    alert("Please select at least one character type for your password");
     return;
   }
-
-  alert("Please add lowercase, uppercase,  ")
-  var lowercase = confirm("Would you like to include lower case characters?");
   
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num = "0123456789";
+  var specialCharacters = "!@#$%^&*()_-+=<>?/\|}{;:";
+
+  var allCharacters = lowerCase + upperCase + num + specialCharacters;
+
+
   
-
-
-
-
 }
-
-
-
 
 function writePassword() {
   var password = generatePassword();
